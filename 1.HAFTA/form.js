36c9281f0ced851680ad388a5b7a86868973633a
aykut <73406165+atmacaaykut@users.noreply.github.com>
2022-02-24 01:19:21 +0300

    v3

--- 1.HAFTA/form.js
+++ 1.HAFTA/form.js
@@ -1,15 +1,24 @@
 let kayit = [];
+    let ad=document.getElementById("ad");
+    let posta=document.getElementById("posta");
+    let tel=document.getElementById("tel");
+    let cinsiyet=document.getElementById("cinsiyet");
+    let calisma=document.getElementById("calisma");
+    let sehir=document.getElementById("sehir");
+    let bolum=document.getElementById("bolum");
+    let alan=document.getElementById("alan");
+
 function kaydet() {
 
     let sozluk = {
-        "ad" : document.getElementById("ad"),
-        "posta": document.getElementById("posta"),
-        "tel": document.getElementById("tel"),
-        "cinsiyet": document.getElementById("cinsiyet"),
-        "calisma": document.getElementById("calisma"),
-        "sehir": document.getElementById("sehir"),
-        "bolum": document.getElementById("bolum"),
-        "alan": document.getElementById("alan")
+        "ad" : ad.value,
+        "posta": posta.value,
+        "tel": tel.value,
+        "cinsiyet":cinsiyet.value,
+        "calisma": calisma.value,
+        "sehir": sehir.value,
+        "bolum": bolum.value,
+        "alan": alan.value
     }
     kayit.push(sozluk);
     // document.getElementById("frm").reset();
